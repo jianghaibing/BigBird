c4c5221c4d2adbb8e95cca4692ff4efedc2affab
jianghaibing <jianghaibing83@gmail.com>
2015-12-22 01:25:32 +0800

    修改ipad图片

--- BigBird/AppDelegate.swift
+++ BigBird/AppDelegate.swift
@@ -65,7 +65,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(application: UIApplication, supportedInterfaceOrientationsForWindow window: UIWindow?) -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone {
+            return .Portrait
+        }else{
+            return .All
+        }
+    }
 
 }
 
